68f41c65dd672e512bcc851783132245dec6a15c
Remel Kabir <remelkabir@gmail.com>
2018-05-08 22:00:36 -0400

    two screen fucntionality working, there's still work need to be done

--- src/components/sginUp/SignUp.js
+++ src/components/sginUp/SignUp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Dimensions, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Button, Dimensions, StyleSheet, Text, TextInput, TouchableOpacity, View, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import PropTypes from 'prop-types'
 
 // Styles
@@ -46,6 +46,20 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: 'bold',
   },
+  logInButton: {
+    width: Dimensions.get('window').width * 0.13,
+    paddingLeft: 18,
+    paddingBottom: 5,
+    backgroundColor: '#fff',
+    borderRadius: 30,
+  },
+  logInArrow: {
+    color: '#c70039',
+    fontSize: 35
+  },
+  logInButtonWrapper: {
+    alignSelf: 'flex-end'
+  },
 })
 // end of styles
 
@@ -62,6 +76,10 @@ const Tcpp = ({ children }) => (
 // Main Component
 export class SignUp extends Component {
   state = {
+    firstHalf: true,
+    fname: '',
+    lname: '',
+    dob: '',
     email: '',
     password: '',
     confirmPass: ''
@@ -78,10 +96,23 @@ export class SignUp extends Component {
     },
   }
 
-  render() {
-    return (
-      <View style={styles.container}>
+  handleSignUp = () => {
+    const { fname, lname, dob, email, password, confirmPass } = this.state
+    alert(`First Name: ${fname}\n
+          LastName: ${lname}\n
+          DOB: ${dob}\n
+          email: ${email}\n
+          password: ${password}\n
+          confirmPass: ${confirmPass}
+          `
+        )
+  }
 
+  render() {
+    const { firstHalf, fname, lname, dob, email, password, confirmPass } = this.state
+    return (
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
         <View style={[styles.fbButton, styles.row]}>
           <Button
             title="SIGN UP WITH FACEBOOK"
@@ -95,68 +126,118 @@ export class SignUp extends Component {
         </View>
 
         <View>
-          {/* eslint-disable*/}
-          <TextInput
-            style={[styles.row, styles.textInput]}
-            onChangeText={email => this.setState({ email })}
-            value={this.state.email}
-            placeholder="Email"
-            placeholderTextColor="#fff"
-            returnKeyType="next"
-            onSubmitEditing={(event) => {
-              this.refs.password.focus();
-            }}
-          />
-          <TextInput
-            ref='password'
-            style={[styles.row, styles.textInput]}
-            onChangeText={password => this.setState({ password })}
-            value={this.state.password}
-            placeholder="Password"
-            placeholderTextColor="#fff"
-            returnKeyType="next"
-            onSubmitEditing={(event) => {
-              this.refs.confirmPassword.focus();
-            }}
-            secureTextEntry
-          />
-          <TextInput
-            ref='confirmPassword'
-            style={[styles.row, styles.textInput]}
-            onChangeText={confirmPass => this.setState({ confirmPass })}
-            value={this.state.confirmPass}
-            placeholder="Confirm password"
-            placeholderTextColor="#fff"
-            returnKeyType="done"
-            secureTextEntry
-          />
-          {/* eslint-enable */}
-        </View>
+          {firstHalf ?
+            <View style={[styles.row, styles.logInButtonWrapper]}>
+              <TextInput
+              style={[styles.row, styles.textInput]}
+              onChangeText={fname => this.setState({ fname })}
+              value={this.state.fname}
+              placeholder="FIRST NAME"
+              placeholderTextColor="#fff"
+              returnKeyType="next"
+              onSubmitEditing={(event) => {
+                this.refs.lname.focus();
+              }}
+              />
+              <TextInput
+              ref='lname'
+              style={[styles.row, styles.textInput]}
+              onChangeText={lname => this.setState({ lname })}
+              value={this.state.lname}
+              placeholder="LAST NAME"
+              placeholderTextColor="#fff"
+              returnKeyType="next"
+              onSubmitEditing={(event) => {
+                this.refs.dob.focus();
+              }}
+              />
+              <TextInput
+              ref='dob'
+              style={[styles.row, styles.textInput]}
+              onChangeText={dob => this.setState({ dob })}
+              value={this.state.dob}
+              placeholder="DATE OF BIRTH"
+              placeholderTextColor="#fff"
+              returnKeyType="done"
+              />
+              <View style={styles.row}/>
 
-        <View style={[styles.row, styles.signButton]}>
-          <Button
-            title="SIGN UP"
-            onPress={() => {}}
-            color="rgb(184, 0, 43)"
-          />
+              <View style={[styles.row, styles.logInButtonWrapper]}>
+                <TouchableOpacity
+                  onPress={() => {this.setState({firstHalf: false})}} // gets the next set of input fields
+                  style={styles.logInButton}
+                >
+                <Text style={styles.logInArrow}>{"\u003E"}</Text>
+                </TouchableOpacity>
+              </View>
+            </View> : 
+            <View>
+              <TextInput
+              style={[styles.row, styles.textInput]}
+              onChangeText={email => this.setState({ email })}
+              value={this.state.email}
+              placeholder="EMAIL"
+              placeholderTextColor="#fff"
+              returnKeyType="next"
+              onSubmitEditing={(event) => {
+                this.refs.password.focus();
+              }}
+              />
+              <TextInput
+                ref='password'
+                style={[styles.row, styles.textInput]}
+                onChangeText={password => this.setState({ password })}
+                value={this.state.password}
+                placeholder="PASSWORD"
+                placeholderTextColor="#fff"
+                returnKeyType="next"
+                onSubmitEditing={(event) => {
+                  this.refs.confirmPassword.focus();
+                }}
+                secureTextEntry
+              />
+              <TextInput
+                ref='confirmPassword'
+                style={[styles.row, styles.textInput]}
+                onChangeText={confirmPass => this.setState({ confirmPass })}
+                value={this.state.confirmPass}
+                placeholder="CONFIRM PASSWORD"
+                placeholderTextColor="#fff"
+                returnKeyType="done"
+                secureTextEntry
+              />
+              <View style={styles.row}/>
+
+              <View style={[styles.row, styles.logInButtonWrapper]}>
+                <TouchableOpacity
+                  onPress={this.handleSignUp}
+                  style={styles.logInButton}
+                >
+                <Text style={styles.logInArrow}>{"\u003E"}</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          }
         </View>
 
         <View style={styles.row} />
-
-        <View style={styles.row}>
-          <Tcpp style={styles}>
-            <Text>By signing up, you agree to our </Text>
-            <TouchableOpacity onPress={() => {}}>
-              <Text style={styles.tols}>Terms &amp; Conditions</Text>
-            </TouchableOpacity>
-            <Text> and </Text>
-            <TouchableOpacity onPress={() => {}}>
-              <Text style={styles.tols}>Privacy Policy</Text>
-            </TouchableOpacity>
-          </Tcpp>
+        {
+          firstHalf ? null :
+          <View style={styles.row}>
+            <Tcpp style={styles}>
+              <Text>By signing up, you agree to our </Text>
+              <TouchableOpacity onPress={() => {}}>
+                <Text style={styles.tols}>Terms &amp; Conditions</Text>
+              </TouchableOpacity>
+              <Text> and </Text>
+              <TouchableOpacity onPress={() => {}}>
+                <Text style={styles.tols}>Privacy Policy</Text>
+              </TouchableOpacity>
+            </Tcpp>
+          </View>
+        }
         </View>
-
-      </View>
+      </TouchableWithoutFeedback>
     )
   }
 }
