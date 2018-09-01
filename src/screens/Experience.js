8f0eb7e63ba09fb89707c7ad92327e37f9105550
David <davidgcastro93@gmail.com>
2018-09-01 14:48:44 -0400

    starting experience screen

--- src/screens/Experience.js
+++ src/screens/Experience.js
@@ -1,9 +1,29 @@
+import { Dimensions, ScrollView, Image, View, Text } from 'react-native'
 import React from 'react'
-import { Text } from 'react-native'
+import { LinearGradient } from 'expo'
+import experiences from '../experiences'
+const { width, height } = Dimensions.get('window')
+let experience = experiences[0]
+export default class Experience extends React.Component {
+  render () {
+    return (
+      <View>
+        <ScrollView
+          horizontal pagingEnabled >
+          <LinearGradient
+            style={{flex: 1}}
+            colors={['rgba(255, 255, 255, 0)', 'rgba(0,0,0,1)']}
 
-const Experience = props => {
-  console.log(props)
-  return <Text>HELLO</Text>
+          >
+            <Image source={experience.images[0]} style={{ height, width }} />
+          </LinearGradient>
+        </ScrollView>
+        <View style={{ position: 'absolute' }}>
+          <Text>Page Content</Text>
+        </View>
+      </View>
+    )
+  }
 }
 
-export default Experience
+// source = { experience.media }
