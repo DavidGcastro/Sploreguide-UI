9df2393df6940235867d0f943eec74128c69e560
David <davidgcastro93@gmail.com>
2018-09-01 13:33:15 -0400

    slight changes

--- src/screens/Experience.js
+++ src/screens/Experience.js
@@ -3,7 +3,6 @@ import { Text } from 'react-native'
 
 export default class Experience extends Component {
   render () {
-    console.log(this.props.item)
     return <Text>Hello World</Text>
   }
 }
