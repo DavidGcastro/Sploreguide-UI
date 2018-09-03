73db84fd7e779c81beb09fc99202bde3eaa2d99b
David <davidgcastro93@gmail.com>
2018-09-03 17:16:43 -0400

    moved content to another file

--- src/components/ExperienceFullScreen.js
+++ src/components/ExperienceFullScreen.js
@@ -1,30 +1,53 @@
 import React from 'react'
 import { Dimensions, View, Text, TouchableOpacity } from 'react-native'
 import landingStyles from '../styles/landingStyles'
-import { Ionicons, SimpleLineIcons, Feather } from '@expo/vector-icons'
+import { Ionicons, SimpleLineIcons, Feather, MaterialIcons } from '@expo/vector-icons'
+import { ifIphoneX } from 'react-native-iphone-x-helper'
+
+let iphoneTop = {
+  ...ifIphoneX(
+    {
+      top: 10
+
+    },
+    {
+      top: 10
+    }
+  )
+}
 
 const { width, height } = Dimensions.get('window')
 
 const ExperienceFullScreen = props => {
   let {item} = props
-  return <View style={{
-    position: 'absolute',
-    flex: 1,
-    justifyContent: 'space-between',
-    width,
-    height
-  }}>
+  return <View
+    pointerEvents='box-none'
+    style={{
+      position: 'absolute',
+      flex: 1,
+      justifyContent: 'space-between',
+      width,
+      height,
+      top: iphoneTop.top,
+      bottom: iphoneTop.bottom
+
+    }}>
     <View style={[landingStyles.topContainer]}>
-      <Feather
-        name='arrow-left'
-        size={32}
-        color='white'
-      />
-      <Text style={landingStyles.activityType}>
-        {item.activityType.toUpperCase()}
-      </Text>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
+          <Feather
+            name='arrow-left'
+            size={32}
+            color='white'
+            style={{ marginRight: 20 }}
+          />
+        </TouchableOpacity>
+        <Text style={landingStyles.activityType}>
+          {item.activityType.toUpperCase()}
+        </Text>
+      </View>
       <Text style={landingStyles.price}>
-                ${item.basePricePerPerson}
+        ${item.basePricePerPerson}
       </Text>
     </View>
     {/* --------------------- */}
@@ -51,13 +74,16 @@ const ExperienceFullScreen = props => {
         </TouchableOpacity>
       </View>
       <View style={[landingStyles.bottomContainer, { justifyContent: 'space-evenly', height: height / 2 - 50 }]}>
-        <View
-          style={{
-            borderBottomColor: 'rgba(255, 255, 255, .3)',
-            borderBottomWidth: 1
-          }}>
+        <View>
           <Text style={landingStyles.location}>{item.location}</Text>
           <Text style={landingStyles.title}>{item.title}</Text>
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View style={{ height: 1, width: 50, backgroundColor: 'rgba(255, 255, 255, .5)' }} />
+          <View style={{ height: 1, width: 50, backgroundColor: 'rgba(255, 255, 255, .5)' }} />
+          <View style={{ height: 1, width: 50, backgroundColor: 'rgba(255, 255, 255, .5)' }} />
+          <View style={{ height: 1, width: 50, backgroundColor: 'rgba(255, 255, 255, .5)' }} />
+          <View style={{ height: 1, width: 50, backgroundColor: 'rgba(255, 255, 255, .5)' }} />
         </View>
         <View style={landingStyles.lastContainer}>
           <View style={landingStyles.iconTextContainer}>
@@ -68,7 +94,7 @@ const ExperienceFullScreen = props => {
             />
             <Text style={landingStyles.smallTextBottom}>
               {Math.round((item.duration / 60) % 60)}
-                            :00
+              :00
             </Text>
           </View>
           <View style={landingStyles.iconTextContainer}>
@@ -92,8 +118,28 @@ const ExperienceFullScreen = props => {
             </Text>
           </View>
         </View>
-        <Text style={{ color: 'white' }}>Free Shots, Free Food</Text>
-        <Text style={{ color: 'white' }}>{item.description}</Text>
+        <View style={{ flexDirection: 'row' }}>
+          <MaterialIcons
+            name='playlist-add'
+            size={18}
+            style={{
+              paddingRight: 10,
+              color: 'rgba(132, 146, 166, 1)'
+            }}
+          />
+          <Text style={{ color: 'white' }}>Free Shots, Free Food</Text>
+        </View>
+        <View style={{ flexDirection: 'row', marginRight: 20 }}>
+          <Ionicons
+            name='ios-person-outline'
+            size={18}
+            style={{
+              paddingRight: 10,
+              color: 'rgba(132, 146, 166, 1)'
+            }}
+          />
+          <Text style={{ color: 'white' }}>{item.description}</Text>
+        </View>
       </View>
     </View>
   </View>
--- src/screens/Experience.js
+++ src/screens/Experience.js
@@ -1,203 +1,34 @@
-import { Dimensions, ScrollView, Image, View, Text, TouchableOpacity, SafeAreaView } from 'react-native'
+import { Dimensions, ScrollView, Image, View, Text } from 'react-native'
 import React from 'react'
 import { LinearGradient } from 'expo'
-import landingStyles from '../styles/landingStyles'
-import { ifIphoneX } from 'react-native-iphone-x-helper'
-import { Ionicons, SimpleLineIcons, Feather, MaterialIcons } from '@expo/vector-icons'
+import ExperienceFullScreen from '../components/ExperienceFullScreen'
 const { width, height } = Dimensions.get('window')
-
-let iphoneTop = {
-  ...ifIphoneX(
-    {
-      top: 10
-
-    },
-    {
-      top: 10
-    }
-  )
-}
 export default class Experience extends React.Component {
   render () {
     let item = this.props.navigation.state.params.experience
 
     return (
-      <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1}}>
         <ScrollView
           bounces={false}
           style={{ width, height }}
           horizontal pagingEnabled >
-          <LinearGradient
-            colors={['white', 'rgba(0,0,0,1)']}
-            start={[0.5, 0.2]}
-            end={[0.2, 1.0]}
-            style={{
-              flex: 1,
-              width,
-              height
-            }}
-          >
-            <Image source={item.media} style={{ height, width, flex: 1 }} />
-          </LinearGradient>
-          <LinearGradient
-            colors={['white', 'rgba(0,0,0,1.0)']}
-            start={[0.5, 0.2]}
-            end={[0.5, 1.0]}
-            style={{
-              width, height
-            }}
-          >
-            <Image source={item.images[0]} style={{ height, width }} />
-          </LinearGradient>
-          <LinearGradient
-            colors={['white', 'rgba(0,0,0,1.0)']}
-            start={[0.5, 0.2]}
-            end={[0.5, 1.0]}
-            style={{
-              width, height
-
-            }}>
-            <Image source={item.images[1]} style={{ height, width }} />
-          </LinearGradient>
-          <LinearGradient
-            colors={['white', 'rgba(0,0,0,1.0)']}
-            start={[0.5, 0.2]}
-            end={[0.5, 1.0]}
-            style={{
-              width, height
-
-            }}
-          >
-            <Image source={item.images[2]} style={{ height, width }} />
-          </LinearGradient>
+          <Image source={item.media} style={{ height, width }} />
+          <Image source={item.images[0]} style={{ height, width }} />
+          <Image source={item.images[1]} style={{ height, width }} />
+          <Image source={item.images[2]} style={{ height, width }} />
         </ScrollView>
-        <View
-          pointerEvents='box-none'
-          style={{
-            position: 'absolute',
-            flex: 1,
-            justifyContent: 'space-between',
-            width,
-            height,
-            top: iphoneTop.top,
-            bottom: iphoneTop.bottom
-
-          }}>
-          <View style={[landingStyles.topContainer]}>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
-                <Feather
-                  name='arrow-left'
-                  size={32}
-                  color='white'
-                  style={{ marginRight: 20 }}
-                />
-              </TouchableOpacity>
-              <Text style={landingStyles.activityType}>
-                {item.activityType.toUpperCase()}
-              </Text>
-            </View>
-            <Text style={landingStyles.price}>
-              ${item.basePricePerPerson}
-            </Text>
-          </View>
-          {/* --------------------- */}
-          <View style={{
-            left: 0,
-            right: 0,
-            top: 0
-          }}>
-            <View style={landingStyles.bottomContainerIcons}>
-              <TouchableOpacity>
-                <Ionicons
-                  name={'ios-share-outline'}
-                  size={25}
-                  color={'white'}
-                  style={{ paddingRight: 15 }}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Ionicons
-                  name={'ios-heart-outline'}
-                  size={25}
-                  color={'white'}
-                />
-              </TouchableOpacity>
-            </View>
-            <View style={[landingStyles.bottomContainer, { justifyContent: 'space-evenly', height: height / 2 - 50 }]}>
-              <View>
-                <Text style={landingStyles.location}>{item.location}</Text>
-                <Text style={landingStyles.title}>{item.title}</Text>
-              </View>
-              <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                <View style={{ height: 2, width: 50, backgroundColor: 'rgba(255, 255, 255, .5)' }} />
-                <View style={{ height: 2, width: 50, backgroundColor: 'rgba(255, 255, 255, .5)' }} />
-                <View style={{ height: 2, width: 50, backgroundColor: 'rgba(255, 255, 255, .5)' }} />
-                <View style={{ height: 2, width: 50, backgroundColor: 'rgba(255, 255, 255, .5)' }} />
-                <View style={{ height: 2, width: 50, backgroundColor: 'rgba(255, 255, 255, .5)' }} />
-              </View>
-
-              <View style={landingStyles.lastContainer}>
-
-                <View style={landingStyles.iconTextContainer}>
-                  <SimpleLineIcons
-                    name='hourglass'
-                    size={12}
-                    color='white'
-                  />
-                  <Text style={landingStyles.smallTextBottom}>
-                    {Math.round((item.duration / 60) % 60)}
-                    :00
-                  </Text>
-                </View>
-                <View style={landingStyles.iconTextContainer}>
-                  <Ionicons
-                    name='ios-chatbubbles-outline'
-                    size={12}
-                    color='white'
-                  />
-                  <Text style={landingStyles.smallTextBottom}>
-                    {item.languages}
-                  </Text>
-                </View>
-                <View style={landingStyles.iconTextContainer}>
-                  <Ionicons name='ios-star' size={12} color='white' />
-                  <Ionicons name='ios-star' size={12} color='white' />
-                  <Ionicons name='ios-star' size={12} color='white' />
-                  <Ionicons name='ios-star' size={12} color='white' />
-                  <Ionicons name='ios-star-half' size={12} color='white' />
-                  <Text style={landingStyles.smallTextBottom}>
-                    {item.reviews}
-                  </Text>
-                </View>
-              </View>
-              <View style={{flexDirection: 'row'}}>
-                <MaterialIcons
-                  name='playlist-add'
-                  size={18}
-                  style={{
-                    paddingRight: 10,
-                    color: 'rgba(132, 146, 166, 1)'
-                  }}
-                />
-                <Text style={{ color: 'white' }}>Free Shots, Free Food</Text>
-              </View>
-              <View style={{ flexDirection: 'row' }}>
-                <Ionicons
-                  name='ios-person-outline'
-                  size={18}
-                  style={{
-                    paddingRight: 10,
-                    color: 'rgba(132, 146, 166, 1)'
-                  }}
-                />
-                <Text style={{ color: 'white' }}>{item.description}</Text>
-              </View>
-            </View>
-          </View>
-        </View>
-      </SafeAreaView>
+        <ExperienceFullScreen item={item} />
+      </View>
 
     )
   }
 }
+
+// <LinearGradient
+//   pointerEvents='none'
+//   colors={['transparent', 'rgba(0,0,0,1.0)']}
+//   start={[0.5, 0.2]}
+//   end={[0.5, 1.0]}
+//   style={{ height }}
+// />
