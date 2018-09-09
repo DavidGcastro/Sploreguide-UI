9311477bf36f873a8b0103ff573c8c685d487b2e
David <davidgcastro93@gmail.com>
2018-09-09 14:53:13 -0400

    Finished refactor

--- src/screens/Experience.js
+++ src/screens/Experience.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import {ScrollView, View, Text, Dimensions} from 'react-native'
 import ExperienceFullScreen from '../components/ExperienceFullScreen'
+import experiences from '../experiences'
+let {width, height} = Dimensions.get('window')
 
 export default class Experience extends React.Component {
   render () {
-    const item = this.props.navigation.state.params.experience
+    // const item = this.props.navigation.state.params.experience
+    const item = experiences[0]
     const nav = this.props.navigation.goBack
     return (
-      <ExperienceFullScreen item={item} nav={nav} />
+      <View style={{flex: 1}}>
+        <ExperienceFullScreen item={item} nav={nav} />
+      </View>
+
     )
   }
 }
