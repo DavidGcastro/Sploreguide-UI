a5de37662fda52562ab41acb876ee511fdf3c865
David <davidgcastro93@gmail.com>
2018-09-10 20:03:31 -0400

    exploreinfosection tweaks

--- src/components/ExploreInfoSection.js
+++ src/components/ExploreInfoSection.js
@@ -2,28 +2,35 @@ import React from 'react'
 import {Text, View, Image} from 'react-native'
 
 const ExploreInfoSection = props => {
-  let header = props.hostImage ? <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10 }}>
-    <Text style={{
-      fontSize: 14,
-      fontFamily: 'SF-UI-Text-Semibold',
-      color: 'rgba(36, 37, 61, 1)'
-    }}>{props.heading}</Text>
-    <Image source={props.hostImage} style={{ width: 50, height: 50, borderRadius: 25 }} />
-  </View> : <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10 }}>
-    <Text style={{
-      fontSize: 14,
-      fontFamily: 'SF-UI-Text-Semibold',
-      color: 'rgba(36, 37, 61, 1)'
-    }}>{props.heading}</Text></View>
+  let header = props.hostImage
+    ? <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+      <Text style={{
+        fontSize: 14,
+        fontFamily: 'SF-UI-Text-Semibold',
+        color: 'rgba(36, 37, 61, 1)'
+      }}>{props.heading}</Text>
+      <Image source={props.hostImage} style={{ width: 50, height: 50, borderRadius: 25 }} />
+    </View>
+    : <View style={{ justifyContent: 'space-between' }} >
+      <Text style={{
+        fontSize: 14,
+        fontFamily: 'SF-UI-Text-Semibold',
+        color: 'rgba(36, 37, 61, 1)'
+      }}>{props.heading}</Text></View>
 
   return <View style={{
-    margin: 20, justifyContent: 'space-between', flex: 1, borderColor: 'rgba(151, 151, 151, .3)', borderBottomWidth: 1, paddingBottom: 20
-  }}>{header}
+    margin: 20,
+    justifyContent: 'space-between',
+    borderColor: 'rgba(151, 151, 151, .3)',
+    borderBottomWidth: 1
+  }}>
+    {header}
     <Text style={{
       fontSize: 13,
       fontFamily: 'SF-UI-Text-Semibold',
       color: 'rgba(74, 74, 74, 1)',
-      lineHeight: 17
+      lineHeight: 17,
+      paddingVertical: 20
     }}>{props.content}</Text>
   </View>
 }
