dc4c86e968344ea45231455fe4ecd7a84ec712d3
Kwadwo Nyarko <kwadwon@gmail.com>
2018-09-18 18:02:11 -0400

    remove comments

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -294,47 +294,6 @@ export default class Landing extends Component {
           </View>)}
       >
       </FlatList>
-
-      // <ScrollView
-      //   decelerationRate={0}
-      //   alwaysBounceVertical={false}
-      //   bounces={false}
-      //   snapToInterval={cardHeight.scrollViewInterval}
-      //   showsHorizontalScrollIndicator={false}
-      //   showsVerticalScrollIndicator={false}
-      //   onScroll={x => {
-      //     let currentOffset = x.nativeEvent.contentOffset.y
-      //     let direction = currentOffset >= offset ? 'up' : 'down'
-      //     offset = currentOffset
-
-      //     this.onSwipeUp(currentOffset, direction)
-      //   }}
-      //   scrollEventThrottle={1}
-      // >
-      //   {
-      //     categories.map((category, index) =>
-      //       (<View key={index} style={{ flex: 1, height: cardHeight.height, marginBottom: 20 }}>
-      //         <Query query={GET_EXPERIENCES_BY_CATEGORY} variables={{input: category[1]}}>
-      //           {({loading, error, data}) => {
-      //             if (loading) return 'Loading...'
-      //             if (error) return `Error! ${error.message}`
-      //             let exps = data.getExperiences
-      //             defaultImageCount += exps.length
-      //             return (
-      //               <Carousel
-      //                 data={exps}
-      //                 renderItem={this._renderItem}
-      //                 sliderWidth={width}
-      //                 extraData={favorites}
-      //                 itemWidth={width - 50}
-      //                 removeClippedSubviews
-      //               />
-      //             )
-      //           }}
-      //         </Query>
-      //       </View>))
-      //   }
-      // </ScrollView>
     )
 
     return (
