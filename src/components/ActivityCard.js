bdb469dbdbfae74df26e193d185e8c7f8e25e997
David <davidgcastro93@gmail.com>
2018-10-17 02:35:32 -0400

    finished added icons, refactored activity card code

--- src/components/ActivityCard.js
+++ src/components/ActivityCard.js
@@ -1,86 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import styles from '../styles/activityCard'
-import {
-  MaterialCommunityIcons,
-  FontAwesome,
-  MaterialIcons,
-  Entypo
-} from '@expo/vector-icons'
-import findActivityIcon from '../helpers/activityTypeIcon'
+import createComponent from '../helpers/activityTypeIcon'
 
 const ActivityCard = props => {
-  let { activity, label } = props
-  let { IconTag, iconName } = findActivityIcon(activity)
-  let Component = ''
-
-  if (IconTag === 'Feather') {
-    Component = (
-      <Feather
-        size={35}
-        name={iconName}
-        style={{
-          color: 'rgba(132, 146, 166, 1)'
-        }}
-      />
-    )
-  } else if (IconTag === 'MaterialCommunityIcons') {
-    Component = (
-      <MaterialCommunityIcons
-        size={35}
-        name={iconName}
-        style={{ color: 'rgba(132, 146, 166, 1)' }}
-      />
-    )
-  } else if (IconTag === 'Entypo') {
-    Component = (
-      <Entypo
-        size={35}
-        name={iconName}
-        style={{ color: 'rgba(132, 146, 166, 1)' }}
-      />
-    )
-  } else if (IconTag === 'MaterialIcons') {
-    Component = (
-      <MaterialIcons
-        size={35}
-        name={iconName}
-        style={{ color: 'rgba(132, 146, 166, 1)' }}
-      />
-    )
-  } else if (IconTag === 'Ionicons') {
-    Component = (
-      <Ionicons
-        size={35}
-        name={iconName}
-        style={{ color: 'rgba(132, 146, 166, 1)' }}
-      />
-    )
-  } else if (IconTag === 'FontAwesome') {
-    Component = (
-      <FontAwesome
-        size={35}
-        name={iconName}
-        style={{ color: 'rgba(132, 146, 166, 1)' }}
-      />
-    )
-  } else if (IconTag === 'SimpleLineIcons') {
-    Component = (
-      <SimpleLineIcons
-        size={35}
-        name={iconName}
-        style={{ color: 'rgba(132, 146, 166, 1)' }}
-      />
-    )
-  } else if (IconTag === 'EvilIcons') {
-    Component = (
-      <EvilIcons
-        size={35}
-        name={iconName}
-        style={{ color: 'rgba(132, 146, 166, 1)' }}
-      />
-    )
-  }
+  let Component = createComponent(props.activity)
   return (
     <View style={styles}>
       {Component}
@@ -89,7 +13,7 @@ const ActivityCard = props => {
           color: 'rgba(132, 146, 166, 1)',
           fontSize: 12
         }}>
-        {label}
+        {props.label}
       </Text>
     </View>
   )
--- src/helpers/activityTypeIcon.js
+++ src/helpers/activityTypeIcon.js
@@ -6,69 +6,61 @@ import {
   Entypo
 } from '@expo/vector-icons'
 import React from 'react'
-export default createComponent;
-
 
 const createComponent = activity => {
-let {IconTag, iconName} = findActivityIcon(activity)
-let Component;
-switch (IconTag) {
+  let { IconTag, iconName } = findActivityIcon(activity)
+
+  switch (IconTag) {
     case 'MaterialCommunityIcons':
-      return Component = (
-        <MaterialCommunityIcons
-          size={35}
-          name={iconName}
-          style={{
-            color: 'rgba(132, 146, 166, 1)'
-          }}
-        />)
-    case 'MaterialIcons': 
-    return Component = (
-      <MaterialIcons
+      return <MaterialCommunityIcons
         size={35}
         name={iconName}
         style={{
           color: 'rgba(132, 146, 166, 1)'
         }}
-      />)
-    case 'FontAwesome': 
-    return Component = (
-      <FontAwesome
+      />
+    case 'MaterialIcons':
+      return <MaterialIcons
         size={35}
         name={iconName}
         style={{
           color: 'rgba(132, 146, 166, 1)'
         }}
-      />)
-    case 'Entypo': 
-    return Component = (
-      <Entypo
+      />
+    case 'FontAwesome':
+      return <FontAwesome
         size={35}
         name={iconName}
         style={{
           color: 'rgba(132, 146, 166, 1)'
         }}
-      />)
-
+      />
+    case 'Entypo':
+      return <Entypo
+        size={35}
+        name={iconName}
+        style={{
+          color: 'rgba(132, 146, 166, 1)'
+        }}
+      />
+  }
 }
-
 
 const findActivityIcon = (activity) => {
   switch (activity) {
     case 'Historical': return { IconTag: 'MaterialIcons', iconName: 'history' }
-    case 'Nature': return {IconTag: 'MaterialCommunityIcons', iconName: 'pine-tree'}
+    case 'Nature': return { IconTag: 'MaterialCommunityIcons', iconName: 'pine-tree' }
     case 'Sports': return {
-      IconTag: 'FontAwesome', iconName: 'soccer-ball-o' }
+      IconTag: 'FontAwesome', iconName: 'soccer-ball-o'
+    }
     case 'Nightlife': return { IconTag: 'Entypo', iconName: 'drink' }
-    case 'Museums': return {IconTag: 'FontAwesome', iconName: 'building-o'}
+    case 'Museums': return { IconTag: 'FontAwesome', iconName: 'building-o' }
     case 'Educational': return { IconTag: 'EvilIcons', iconName: 'pencil' }
     case 'Shopping': return { IconTag: 'MaterialIcons', iconName: 'shopping-basket' }
     case 'Food': return { IconTag: 'MaterialCommunityIcons', iconName: 'food-variant' }
     case 'Arts': return { IconTag: 'MaterialCommunityIcons', iconName: 'drawing-box' }
-    default: return {IconTag: 'MaterialCommunityIcons', iconName: 'map-search-outline'}
+    default: return { IconTag: 'MaterialCommunityIcons', iconName: 'map-search-outline' }
   }
 }
 
-
-
-
+export default createComponent
